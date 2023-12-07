9f483dbcfd4bca02d11c34b9cebbe9bb44d28edf
testeryup <92677873+testeryup@users.noreply.github.com>
2023-12-07 16:37:49 +0700

    Update Đếm số lượng cặp số bằng nhau.cpp

--- Mang 1 chieu nang cao/Đếm số lượng cặp số bằng nhau.cpp
+++ Mang 1 chieu nang cao/Đếm số lượng cặp số bằng nhau.cpp
@@ -1,23 +1,30 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<cmath>
+#include<sstream>
+#include<unordered_map>
+
 using namespace std;
+using ll = long long;
 
-int countPairs(int arr[], int n) {
-    unordered_map<int, int> freq;
-    // Finding frequency of each number.
-    for (int i = 0; i < n; i++)
-        freq[arr[i]]++;
-    // Calculating pairs of each value.
-    int ans = 0;
-    for (auto it : freq) {
-        int count = it.second;
-        ans += count * (count - 1) / 2;
+int d[1000005] = {0};
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for(int &x : a){
+        cin >> x;
+        d[x]++;
     }
-    return ans;
-}
 
-int main() {
-    int arr[] = {2, 2, 1, 3, 2, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << countPairs(arr, n) << endl;
+    ll cnt = 0;
+
+    for(int val = 0; val <= 1e6; ++val){
+        cnt += 1ll * d[val] * (d[val] - 1) / 2;
+    }
+    cout << cnt;
     return 0;
 }
