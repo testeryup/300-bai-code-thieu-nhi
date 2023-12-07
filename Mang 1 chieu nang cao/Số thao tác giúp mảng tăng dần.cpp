d118247ad462c2dc1e29f475c4e0ea1fddd77a73
testeryup <92677873+testeryup@users.noreply.github.com>
2023-12-07 16:05:35 +0700

    Create Số thao tác giúp mảng tăng dần.cpp

---
+++ Mang 1 chieu nang cao/Số thao tác giúp mảng tăng dần.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<cmath>
+#include<sstream>
+#include<unordered_map>
+
+using namespace std;
+using ll = long long;
+bool cmp(const int &a, const int &b){
+    return a > b;
+}
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for(int &x : a) cin >> x;
+    int ans = 0;
+    for(int i = 1; i < n; ++i){
+        if(a[i] < a[i - 1]){
+            ans += (a[i - 1] + 1 - a[i]);
+        }
+    }
+    cout << ans;
+    return 0;
+}
