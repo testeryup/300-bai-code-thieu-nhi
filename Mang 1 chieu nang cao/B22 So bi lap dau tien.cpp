f47a84a7dc180410f3c9b3552e47611e1f533ae1
testeryup <92677873+testeryup@users.noreply.github.com>
2023-12-07 15:10:49 +0700

    Create B22 So bi lap dau tien.cpp

---
+++ Mang 1 chieu nang cao/B22 So bi lap dau tien.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<cmath>
+#include<sstream>
+#include<unordered_map>
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for(int &x : a) cin >> x;
+    // bool isLooped = false;
+    unordered_map<int, int> mp;
+    for(const int &x : a){
+        mp[x]++;
+        if(mp[x] > 1){
+            cout << x;
+            return 0;
+        }
+    }
+    cout << -1;
+    return 0;
+}
