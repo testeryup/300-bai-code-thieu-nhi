c048494e0d393eb86a28d0c212819d03910e4a57
testeryup <92677873+testeryup@users.noreply.github.com>
2023-12-21 20:53:21 +0700

    Create Bài 6. Ký tự xuất hiện ở 2 xâu 2.cpp

---
+++ string/Bài 6. Ký tự xuất hiện ở 2 xâu 2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<ctype.h>
+#include<algorithm>
+#include<unordered_set>
+#include<vector>
+#include<set>
+
+
+using namespace std;
+
+int main(){
+    string s1, s2; cin >> s1; cin >> s2;
+    string res1, res2;
+    int cnt1[256] = {0}, cnt2[256] = {0};
+
+    for(const char &x : s1){
+        if(cnt1[x] == 0){
+            ++cnt1[x];
+        }
+    }
+
+    for(const char &x : s2){
+        if(cnt1[x] == 0){
+            ++cnt2[x];
+        }
+        else
+            cnt1[x] = 2;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if(cnt1[i] == 1)
+            res1.push_back((char) i);
+        
+        if(cnt2[i] == 1)
+            res2.push_back((char) i);
+
+    }
+    
+    cout << res1 << '\n' << res2;
+    return 0;
+}
